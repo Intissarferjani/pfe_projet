903b1235f8cab067af130c12046b9c96f484fdfd
intissar <ferjaniintissar8@gmail.com>
2024-02-15 14:15:38 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { ProduitComponent } from './produit/produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:Page1Component},
+  {path:'produit',component:ProduitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { ProduitComponent } from './produit/produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    ProduitComponent
   ],
   imports: [
     BrowserModule,
